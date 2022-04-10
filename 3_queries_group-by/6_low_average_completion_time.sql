eaf92b95ed42e5bc236fe358f2b8ff3d4031dc6d
Liam Smith <liam.s.smith@gmail.com>
2022-04-10 12:31:58 +0000

    add low_average_completion_time

--- 3_queries_group-by/6_low_average_completion_time.sql
+++ 3_queries_group-by/6_low_average_completion_time.sql
@@ -4,4 +4,4 @@ JOIN students ON students.id = student_id
 JOIN assignments ON assignments.id = assignment_id
 GROUP BY students.id, students.end_date
 HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration) AND students.end_date IS NULL 
-ORDER BY AVG(assignment_submissions.duration)
+ORDER BY AVG(assignment_submissions.duration);
